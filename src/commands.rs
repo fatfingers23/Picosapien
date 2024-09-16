7dde5be84069e9e5f4b2732e99b2e101cbd14a33
Bailey Townsend <baileytownsend2323@gmail.com>
2024-09-16 18:38:00 -0500

    Pickup

---
+++ src/commands.rs
@@ -0,0 +1,8 @@
+pub enum RobotCommand {
+    RightArmUp = 0x81,
+    RightArmDown = 0x84,
+    RightArmOut = 0x82,
+    RightHandPickUp = 0xA4,
+    RoseBud = 0xD1,
+    StartUpWakeUp = 0xB1,
+}
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,23 @@
 #![no_std]
 #![no_main]
 
+use commands::RobotCommand;
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{Level, Output};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
+mod commands;
 mod cyw43_driver;
 
 const CYCLE: u64 = 833;
 
-async fn send_command<'a>(pin: &mut Output<'a>, command: u8) {
+async fn send_command<'a>(pin: &mut Output<'a>, command: RobotCommand) {
+    send_command_raw(pin, command as u8).await;
+}
+
+async fn send_command_raw<'a>(pin: &mut Output<'a>, command: u8) {
     // Pin is set to high and when low for 8 cycles it signifies a start of a command
     pin.set_low();
     Timer::after(Duration::from_micros(CYCLE * 8)).await;
@@ -23,7 +29,6 @@ async fn send_command<'a>(pin: &mut Output<'a>, command: u8) {
     //Convert the command to the 8 bit binary representation
     for i in (0..8).rev() {
         let bit = (command >> i) & 1;
-        println!("bit: {}", bit);
         // Send the high-low sequence based on the bit value
         match bit {
             1 => {
@@ -40,9 +45,6 @@ async fn send_command<'a>(pin: &mut Output<'a>, command: u8) {
             }
             2_u8..=u8::MAX => error!("Invalid bit value"),
         }
-        //TODO test if this is needed
-        pin.set_low();
-        Timer::after(Duration::from_micros(CYCLE * 1)).await;
     }
 
     // Set back to high to end the transmission
@@ -52,19 +54,16 @@ async fn send_command<'a>(pin: &mut Output<'a>, command: u8) {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let delay = Duration::from_secs(5);
+    let delay = Duration::from_secs(15);
     let mut pin = Output::new(p.PIN_16, Level::High);
 
+    //The pin is default as high
+
     pin.set_high();
-    //wake up
-    // send_command(&mut pin, 0xB1).await;
 
     loop {
-        info!("Tilt Left!");
-        send_command(&mut pin, 0x8B).await;
-        Timer::after(Duration::from_secs(3)).await;
-        info!("Tilt Right!");
-        send_command(&mut pin, 0x83).await;
+        //Bends over and picks up the raspberry pi pico
+        send_command(&mut pin, RobotCommand::RightHandPickUp).await;
         Timer::after(delay).await;
     }
 }

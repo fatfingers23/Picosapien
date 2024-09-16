df499c6310539e181806964f60867f5f84c2ff2b
Bailey Townsend <baileytownsend2323@gmail.com>
2024-09-15 23:20:28 -0500

    its finally working

--- src/cyw43_driver.rs
+++ src/cyw43_driver.rs
@@ -36,12 +36,12 @@ pub async fn setup_cyw43<'a>(
 
     // To make flashing faster for development, you may want to flash the firmwares independently
     // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download 43439A0.bin --format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download 43439A0_clm.bin --format bin --chip RP2040 --base-address 0x10140000
-    //     probe-rs download 43439A0_btfw.bin --format bin --chip RP2040 --base-address 0x10141400
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 224190) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
-    //let btfw = unsafe { core::slice::from_raw_parts(0x10141400 as *const u8, 6164) };
+    //     probe-rs download 43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
+    //     probe-rs download 43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
+    //     probe-rs download 43439A0_btfw.bin --binary-format bin --chip RP2040 --base-address 0x10141400
+    // let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 224190) };
+    // let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    // let btfw = unsafe { core::slice::from_raw_parts(0x10141400 as *const u8, 6164) };
 
     let pwr = Output::new(p_23, Level::Low);
     let cs = Output::new(p_25, Level::High);
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,88 @@
-//! This example test the RP Pico W on board LED.
+//! this example test the RP Pico W on board LED.
 //!
 //! It does not work with the RP Pico board.
 
 #![no_std]
 #![no_main]
 
-use cyw43_driver::setup_cyw43;
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::clocks;
+use embassy_rp::gpio::{AnyPin, Level, Output, Pin};
+use embassy_rp::pwm;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 mod cyw43_driver;
+
+const CYCLE: u64 = 833;
+const PERIOD: Duration = Duration::from_micros(CYCLE);
+
+const CODE_RSTurnRight: u8 = 0x80;
+const CODE_RSRightArmUp: u8 = 0x81;
+
+async fn send_command<'a>(pin: &mut Output<'a>, command: u8) {
+    // Send start bit (wf_head)
+    pin.set_low();
+    Timer::after(Duration::from_micros(CYCLE * 8)).await;
+
+    // Send 8 data bits, most significant bit (MSB) first
+    for i in (0..8).rev() {
+        let bit = (command >> i) & 1;
+        println!("bit: {}", bit);
+        // Send the high-low sequence based on the bit value
+        match bit {
+            1 => {
+                pin.set_high();
+                Timer::after(Duration::from_micros(CYCLE * 4)).await;
+                pin.set_low();
+                Timer::after(Duration::from_micros(CYCLE * 1)).await;
+            }
+            0 => {
+                pin.set_high();
+                Timer::after(Duration::from_micros(CYCLE * 1)).await;
+                pin.set_low();
+                Timer::after(Duration::from_micros(CYCLE * 1)).await;
+            }
+            2_u8..=u8::MAX => error!("Invalid bit value"),
+        }
+        pin.set_low();
+        Timer::after(Duration::from_micros(CYCLE * 1)).await;
+    }
+
+    // Send end bit (wf_tail)
+    pin.set_high();
+    // pin.set_low();
+    // Timer::after(wf_tail.1).await;
+
+    // // Set back to high (idle)
+    // pin.set_high();
+}
+
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-    let (_device, mut control) = setup_cyw43(
-        p.PIO0, p.PIN_23, p.PIN_24, p.PIN_25, p.PIN_29, p.DMA_CH0, spawner,
-    )
-    .await;
 
-    let delay = Duration::from_secs(1);
+    let delay = Duration::from_secs(5);
 
+    // let mut pwm = Pwm::new_output_b(p.PWM_SLICE0, p.PIN_17, c.clone());
+    let mut pin = Output::new(p.PIN_16, Level::High);
+
+    pin.set_high();
+    // send_command(&mut pin, 0xEF, wf_head, wf_hi, wf_lo, wf_tail).await;
+    // send_command(&mut pin, 0xB1, wf_head, wf_hi, wf_lo, wf_tail).await;
+    info!("Command sent");
+    // Send the command 0x81
+    // send_command(&mut pin, 0x81, wf_head, wf_hi, wf_lo, wf_tail).await;
     loop {
-        info!("led on!");
-        control.gpio_set(0, true).await;
-        Timer::after(delay).await;
+        // Send the command 0x81
 
-        info!("led off!");
-        control.gpio_set(0, false).await;
+        info!("Sending command");
+        // control.gpio_set(0, true).await;
+        send_command(&mut pin, 0xC0).await;
 
+        info!("waiting");
+        // control.gpio_set(0, false).await;
         Timer::after(delay).await;
     }
 }
